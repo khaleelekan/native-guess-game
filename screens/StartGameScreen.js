60b39f15781899100197b09c269768d6e3711a40
khalilullahi abdulraheem <kalilulekan@gmail.com>
2023-10-07 23:08:22 +0100

    new

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import { PrimaryButton } from "../components/PrimaryButton"
 export const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-        <TextInput/>
+        <TextInput style={numberInput} />
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -23,5 +23,12 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 6,
         shadowOpacity: 0.5
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f'
     }
 })
