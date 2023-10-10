93a15db20c7fe3b213cbf2a81f1f1669df101176
khalilullahi abdulraheem <kalilulekan@gmail.com>
2023-10-10 15:37:38 +0100

    new

---
+++ components/NumberContainer.js
@@ -0,0 +1,26 @@
+import {View, Text,StyleSheet} from 'react-native'
+
+export const NumberContainer = ({children}) => {
+  return (
+    <View style={styles.container}>
+    <Text style={styles.numberText}>{children}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        borderWidth: 4,
+        borderColor: 'white',
+        padding: 24,
+        margin:24,
+        borderRadius: 8,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    numberText:{
+        color: 'white',
+        fontSize: 36,
+        fontWeight: 'bold'
+    }
+})
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,23 @@
 import {View , StyleSheet, Text} from 'react-native'
 import { Title } from '../components/Title'
+import { useState } from 'react';
+import { NumberContainer } from '../components/NumberContainer';
 export const GameScreen = () => {
+  function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  
+    if (rndNum === exclude) {
+      return generateRandomBetween(min, max, exclude);
+    } else {
+      return rndNum;
+    }
+  }
+    const initialGuess = generateRandomBetween(1, 100)
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
   return (
     <View style={styles.container}> 
        <Title> Opponent's Screen</Title>
+       <NumberContainer>{currentGuess}</NumberContainer>
         </View>
   )
 }
